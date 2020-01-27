6d34c0abfbdc2a83fcf27a875d8a0eeb881c9987
MSBAteam1 <60263766+MSBAteam1@users.noreply.github.com>
2020-01-27 16:08:31 -0500

    Add files via upload

---
+++ 6.8SubsetExercise.R
@@ -0,0 +1,84 @@
+# A
+set.seed(1)
+
+X = rnorm(100)
+eps = rnorm(100)
+
+# B
+beta0 = 3
+beta1 = 2
+beta2 = -3
+beta3 = 0.3
+Y = beta0 + beta1 * X + beta2 * X^2 + beta3 * X^3 + eps
+
+# C
+library(leaps)
+data.full = data.frame(y = Y, x = X)
+mod.full = regsubsets(y ~ poly(x, 10, raw = T), data = data.full, nvmax = 10) # trying to find which polynomials are the best predictors for Y
+mod.summary = summary(mod.full)
+
+# Find the model size for best cp, BIC and adjr2
+which.min(mod.summary$cp)
+
+which.min(mod.summary$bic)
+
+which.max(mod.summary$adjr2)
+
+# Plot cp, BIC and adjr2
+plot(mod.summary$cp, xlab = "Subset Size", ylab = "Cp", pch = 20, type = "l")
+points(3, mod.summary$cp[3], pch = 4, col = "red", lwd = 7)
+
+plot(mod.summary$bic, xlab = "Subset Size", ylab = "BIC", pch = 20, type = "l")
+points(3, mod.summary$bic[3], pch = 4, col = "red", lwd = 7)
+
+
+plot(mod.summary$adjr2, xlab = "Subset Size", ylab = "Adjusted R2", pch = 20, 
+     type = "l")
+points(3, mod.summary$adjr2[3], pch = 4, col = "red", lwd = 7)
+
+coefficients(mod.full, id = 3)
+
+# D
+
+mod.fwd = regsubsets(y ~ poly(x, 10, raw = T), data = data.full, nvmax = 10, 
+                     method = "forward")
+mod.bwd = regsubsets(y ~ poly(x, 10, raw = T), data = data.full, nvmax = 10, 
+                     method = "backward")
+fwd.summary = summary(mod.fwd)
+bwd.summary = summary(mod.bwd)
+
+which.min(fwd.summary$cp)
+which.min(bwd.summary$cp)
+
+which.min(fwd.summary$bic)
+which.min(bwd.summary$bic)
+
+which.max(fwd.summary$adjr2)
+which.max(bwd.summary$adjr2)
+
+# Plot the statistics
+par(mfrow = c(1, 1))
+plot(fwd.summary$cp, xlab = "Subset Size", ylab = "Forward Cp", pch = 20, type = "l")
+points(3, fwd.summary$cp[3], pch = 4, col = "red", lwd = 7)
+plot(bwd.summary$cp, xlab = "Subset Size", ylab = "Backward Cp", pch = 20, type = "l")
+points(3, bwd.summary$cp[3], pch = 4, col = "red", lwd = 7)
+plot(fwd.summary$bic, xlab = "Subset Size", ylab = "Forward BIC", pch = 20, 
+     type = "l")
+points(3, fwd.summary$bic[3], pch = 4, col = "red", lwd = 7)
+plot(bwd.summary$bic, xlab = "Subset Size", ylab = "Backward BIC", pch = 20, 
+     type = "l")
+points(3, bwd.summary$bic[3], pch = 4, col = "red", lwd = 7)
+plot(fwd.summary$adjr2, xlab = "Subset Size", ylab = "Forward Adjusted R2", 
+     pch = 20, type = "l")
+points(3, fwd.summary$adjr2[3], pch = 4, col = "red", lwd = 7)
+plot(bwd.summary$adjr2, xlab = "Subset Size", ylab = "Backward Adjusted R2", 
+     pch = 20, type = "l")
+points(4, bwd.summary$adjr2[4], pch = 4, col = "red", lwd = 7)
+
+
+coefficients(mod.fwd, id = 3)
+
+coefficients(mod.bwd, id = 3)
+
+coefficients(mod.fwd, id = 4)
+
